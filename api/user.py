72aa4271deb19a24459c433ee20af78283e05ee8
Ignacio Betegón <i.betegon@going-public.edu>
2021-02-24 13:42:04 +0100

    Add todos

--- api/user.py
+++ api/user.py
@@ -9,6 +9,12 @@ from flask import (
 bp = Blueprint('auth', __name__, url_prefix='/api')
 
 
+# TODO: get all posts from user
+# TODO: get specific post
+# TODO: edit specific post
+# TODO: remove post from user
+# TODO: add like system
+
 @bp.route('/post', methods=['POST'])
 def create_post():
     post = Post(**request.get_json(force=True))
